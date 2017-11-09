37191ec73ff278d60a5820ee3ecd671aef944d7d
andrewdownie <andrew_dev@outlook.com>
2017-11-09 13:48:52 -0500

    Did 2/3's of the write up for Custom_Score, not sure how to explain why it does so well against AB_Improved.

--- game_agent.py
+++ game_agent.py
@@ -10,8 +10,40 @@ class SearchTimeout(Exception):
     """Subclass base exception for code clarity. """
     pass
 
-
+    
 def custom_score(game, player):
+    """Calculate the heuristic value of a game state from the point of view
+    of the given player.
+
+    Note: this function should be called from within a Player instance as
+    `self.score()` -- you should not need to call this function directly.
+
+    Parameters
+    ----------
+    game : `isolation.Board`
+        An instance of `isolation.Board` encoding the current state of the
+        game (e.g., player locations and blocked cells).
+
+    player : object
+        A player instance in the current game (i.e., an object corresponding to
+        one of the player objects `game.__player_1__` or `game.__player_2__`.)
+
+    Returns
+    -------
+
+        The heuristic value of the current game state to the specified player.
+    """
+
+
+    # ---- chase the opponent
+
+    # Try to move as close as possible to where the other player last moved
+    opp_y, opp_x = game.get_player_location(game.get_opponent(player))
+    y, x = game.get_player_location(player)
+    return float((opp_x - y)**2 + (opp_y - x)**2)
+
+
+def custom_score_2(game, player):
     """Calculate the heuristic value of a game state from the point of view
     of the given player.
 
@@ -51,36 +83,6 @@ def custom_score(game, player):
 
 
 
-def custom_score_2(game, player):
-    """Calculate the heuristic value of a game state from the point of view
-    of the given player.
-
-    Note: this function should be called from within a Player instance as
-    `self.score()` -- you should not need to call this function directly.
-
-    Parameters
-    ----------
-    game : `isolation.Board`
-        An instance of `isolation.Board` encoding the current state of the
-        game (e.g., player locations and blocked cells).
-
-    player : object
-        A player instance in the current game (i.e., an object corresponding to
-        one of the player objects `game.__player_1__` or `game.__player_2__`.)
-
-    Returns
-    -------
-
-        The heuristic value of the current game state to the specified player.
-    """
-
-
-    # ---- chase the opponent
-
-    # Try to move as close as possible to where the other player last moved
-    opp_y, opp_x = game.get_player_location(game.get_opponent(player))
-    y, x = game.get_player_location(player)
-    return float((opp_x - y)**2 + (opp_y - x)**2)
 
 
 def custom_score_3(game, player):
